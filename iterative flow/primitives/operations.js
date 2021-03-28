18b3bd29ca5bc720d180862b4928fe341fa02327
Shivarsn <80741230+Shivarsn@users.noreply.github.com>
2021-03-28 20:28:54 +0530

    operations1

---
+++ iterative flow/primitives/operations.js
@@ -0,0 +1,63 @@
+// program to check whether the number is positive or not
+var x = -4;
+if(x>0){
+    console.log("the number is positive number")
+}
+else{
+    console.log("the number is not  a positive number")
+}
+//program to check whether is even or odd
+var x =5;
+if (x%4==0) {
+    console.log("it is a even number")
+    
+} else {
+    console.log("it is a odd number")
+    
+}
+//program to check whether 7 is divisible or not
+var x =10;
+if (x%7==0) {
+    console.log("Divisible by 7")
+    
+} else{
+    console.log("Divisible by 7")
+}
+//program to check is muitiple of 3 or not
+let y=99;
+if(y%3==0){
+    console.log("multiple of 3")
+}
+else{
+    console.log("not a multiple of 3")
+}
+//program to print the greatest number in given two numbers
+let z=7;
+y=6;
+if (z>y) {
+    console.log("greatest number b/w the x and y is,")
+    
+} else{
+    console.log("not a greatest number b/w the x and y is,")
+}
+//program to print least number in b/w two numbers
+let a=-10;
+b=-5;
+if (a<b) {
+    console.log("the given number is a least number")
+} else {
+    console.log("the given number is not least number")
+}
+
+//program to print greatest number in given three numbers
+let d=-1;
+b=-5;
+c=-10;
+console.log("the greatest number in given three numbers is", Math.max(-1,-5,-10))
+
+//program to print least number in given three numbers
+let f=5;
+b=6;
+c=-7;
+console.log("the least number in given three numbers is",Math.min(f,b,c))
+
