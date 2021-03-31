bf06c9b88740c22de4c5c62d5352bd11b193a9f4
Shivarsn <80741230+Shivarsn@users.noreply.github.com>
2021-03-31 13:28:34 +0530

    ok

---
+++ iterative flow/obj properties/ES7/arrow/fatarr.js
@@ -0,0 +1,14 @@
+function MyName (shiva){
+    const MyName='shiva';
+    console.log(MyName);
+}
+MyName();
+//using the fatarrow function
+const name=shiva=>{
+    console.log(shiva);
+}
+name('shiva');
+//for mul function
+const muliply =num=> num*2;
+console.log(muliply(5));
+
